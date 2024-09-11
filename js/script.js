9a7adc6452e08df53372c30489b9593161087a97
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2024-09-10 22:37:58 -0300

    Adicionar a parte visual dos gatinhos

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const listaGatinhos = [
     { nome: "Gato laranja", idade: 4, foto: "./imagens/laranja.png", raca: "Não identificada" },
 ];
 
-
 const exibirGatos = (racaFiltrada) => {
     const secaoGatos = document.getElementById("filter");
     secaoGatos.innerHTML = ""; 
@@ -32,6 +31,25 @@ const exibirGatos = (racaFiltrada) => {
     }
 };
 
+const exibirGatosGeral = (lista, containerId) => {
+    const container = document.getElementById(containerId);
+    container.innerHTML = "";
+
+    for (const gato of lista) {
+        const cardGato = `
+            <article class="card">
+                <img src="${gato.foto}" alt="${gato.nome}" class="card-img"/>
+                <div class="card-content">
+                    <h1>${gato.nome}</h1>
+                    <h2>${gato.idade} anos</h2>
+                    <p>Raça: ${gato.raca}</p>
+                </div>
+            </article>
+        `;
+        container.innerHTML += cardGato;
+    }
+};
+
 const configurarFiltros = () => {
     document.querySelectorAll(".filter-btn").forEach(botao => {
         botao.addEventListener("click", function() {
@@ -48,17 +66,19 @@ const idadeGatinhos = listaGatinhos.reduce((accumulator, gato) => {
     return accumulator += gato.idade;
   }, 0)
 
+  document.getElementById("soma-idades").innerText = `${idadeGatinhos}`;
+
   console.log(idadeGatinhos);
 
 // ------- SPREAD -------
 // Retona a combinação de duas listas de gatinhos
 const listaGatinhosAux = [
-    { nome: "Gato laranja", idade: 5, foto: "./imagens/branco.png", raca: "Munchkin" },
+    { nome: "Gato laranja", idade: 5, foto: "./imagens/laranja.png", raca: "Munchkin" },
     { nome: "Gato cinza", idade: 1, foto: "./imagens/calico.png", raca: "Maine Coon" },
 ]
 
 const listaGatinhosSpread = [
-    { nome: "Gato laranja", idade: 5, foto: "./imagens/branco.png", raca: "Munchkin" },
+    { nome: "Gato laranja", idade: 5, foto: "./imagens/laranja.png", raca: "Munchkin" },
     { nome: "Gato cinza", idade: 1, foto: "./imagens/calico.png", raca: "Maine Coon" },
     ...listaGatinhos
 ]
@@ -67,6 +87,9 @@ console.log(listaGatinhosAux)
 console.log(listaGatinhosSpread)
 
 window.onload = () => {
+    exibirGatosGeral(listaGatinhos, 'gatinhos-cards');
+    exibirGatosGeral(listaGatinhosAux, 'gatinhos-aux-cards');
+    exibirGatosGeral(listaGatinhosSpread, 'gatinhos-spread-cards');
     exibirGatos('all'); 
     configurarFiltros(); 
 };
